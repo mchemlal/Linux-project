e481708be2e848f2ba6f5ded08a3e5fcdfd5b2a3
mchemlal <michel.chemlal@gmail.com>
2021-03-18 13:25:40 +0000

    mise jour du repo pour l'ensemble de l'equipe

--- log_format.sh
+++ log_format.sh
@@ -2,8 +2,11 @@
 
 cat $1 | while read line
 do 
-date=$(echo $line | awk '{print ($1" "$2" " $3)}')
+date=$(echo $line | awk '{print ($1" "$2" ")})
+date -d "`cut -f1 -d. /proc/uptime` ago" -u
 type=$(echo $line | awk '{print ($4)}')
 error_msg=$(echo $line | awk '{print ($5$6$7$8$9$10)}')
 echo {\"date\":\"$date\"','"\"type\":\"$type\",\"error\":\"$error_msg\""}
 done
+
+
