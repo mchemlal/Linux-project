f9cce378391140178372e7323073e9fb3980f162
mchemlal <michel.chemlal@gmail.com>
2021-03-18 14:55:05 +0000

    story 3 completed

--- log_format.sh
+++ log_format.sh
@@ -2,11 +2,8 @@
 
 cat $1 | while read line
 do 
-date=$(echo $line | awk '{print ($1" "$2" ")})
-date -d "`cut -f1 -d. /proc/uptime` ago" -u
-type=$(echo $line | awk '{print ($4)}')
+date=$(echo $line | awk '{print ($1" "$2" ")}' | cut -c2-20)
+type=$(echo $line | awk '{print ($3)}')
 error_msg=$(echo $line | awk '{print ($5$6$7$8$9$10)}')
-echo {\"date\":\"$date\"','"\"type\":\"$type\",\"error\":\"$error_msg\""}
+echo {\"date\":\"$date\"', '"\"type\":\"$type\",\"message\":\"$error_msg\""}
 done
-
-
