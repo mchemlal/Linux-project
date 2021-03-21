c71462be58463187b9be4f7a929320b27f453acb
mchemlal <michel.chemlal@gmail.com>
2021-03-21 16:59:16 +0000

    creation fichier tar.gz

--- log_move.sh
+++ log_move.sh
@@ -5,3 +5,4 @@ date=$(date '+%F-%T')
 echo /var/www/html/log/$date/*.log >> /var/www/html/log/dump_list.txt
 echo /var/www/html/log/$date/*.json >> /var/www/html/log/json_list.txt
 
+tar -czvf log.tar.gz /workspace/Linux-project/
